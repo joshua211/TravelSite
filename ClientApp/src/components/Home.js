0d3159729e66b254c2fd1fe0887627512799bbd1
Lycosh <Lycosh211@gmx.de>
2020-04-27 18:45:48 +0200

    greetings
    Co-authored-by: Martin Zacharias <mrt.zacharias@gmail.com>

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -3,11 +3,17 @@ import Button from "react-bulma-components";
 
 export class Home extends Component {
   static displayName = Home.name;
+  state = { greeting: "" };
+
+  async componentWillMount() {
+    let result = await fetch("/api/Greetings");
+    this.setState({ greeting: await result.text() });
+  }
 
   render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
+        <h1>{this.state.greeting}, world!</h1>
         <p>
           This is a work in progress web <code>application!</code>
         </p>
---
+++ Controllers/GreetingsController.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TravelSite.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GreetingsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetGreeting()
+        {
+            var random = new Random();
+
+            return Greetings[random.Next(3)];
+        }
+
+        public string[] Greetings = {
+            "Hallo",
+            "Hello",
+            "Bonjour"
+        };
+    }
+}
