c081bb85941370d483f3ef7d400c47faa9b2a791
Jan Lappenküper <jan.lappenkueper001@stud.fh-dortmund.de>
2020-05-04 13:44:36 +0200

    Commit Message für Testcommit auf TestBranch

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace TravelSite
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            Console.WriteLine("Test");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
